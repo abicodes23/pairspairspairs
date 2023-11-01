4bfa0324ca00f6e8989b7ce82725a165e618e629
abicodes23 <149591342+abicodes23@users.noreply.github.com>
2023-11-01 20:09:54 +0000

    pairs
    assignment

---
+++ script.js
@@ -0,0 +1,104 @@
+const gridContainer = document.querySelector(".grid-container");
+//global variables// 
+let cards= [];
+//cards array// 
+let firstCard, secondCard; // cards that are compared// 
+let lockBoard = false;
+let moves = 0; //number of moves variable// 
+
+document.querySelector(".moves").textContent = moves;
+
+//fisher-yates shuffle alogorithm//
+//Ronald A. Fisher,Frank Yates., (1938) "Statistical tables for biological, agricultural and medical research"//
+
+fetch("./data/cards.json") //fetch variable the data is located// 
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+  });
+
+function shuffleCards() {
+  let currentIndex = cards.length,
+    randomIndex,
+    temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+}
+
+function generateCards() {
+  for (let card of cards) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.setAttribute("data-name", card.name);
+    cardElement.innerHTML = `
+      <div class="front">
+        <img class="front-image" src=${card.image} />
+      </div>
+      <div class="back"></div>
+    `;
+    gridContainer.appendChild(cardElement);
+    cardElement.addEventListener("click", flipCard);
+  }
+}
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flipped");
+
+  if (!firstCard) {
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  moves++;
+  document.querySelector(".moves").textContent = moves;
+  lockBoard = true;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+}
+
+function restart() {
+  resetBoard();
+  shuffleCards();
+  moves = 0;
+  document.querySelector(".moves").textContent = moves;
+  gridContainer.innerHTML = "";
+  generateCards();
+}
